ff7be898287fcc3a01da47b16b3d210c9ebcf5ef
Edevar Bastreghi Filho <edevarfilho@gmail.com>
2021-02-03 00:11:57 -0300

    BeerServiceTest
    BeerServiceTest

---
+++ src/test/java/one/digitalinnovation/beerstock/builder/service/BeerServiceTest.java
@@ -0,0 +1,52 @@
+package one.digitalinnovation.beerstock.builder.service;
+
+import one.digitalinnovation.beerstock.builder.BeerDTOBuilder;
+import one.digitalinnovation.beerstock.dto.BeerDTO;
+import one.digitalinnovation.beerstock.entity.Beer;
+import one.digitalinnovation.beerstock.exception.BeerAlreadyRegisteredException;
+import one.digitalinnovation.beerstock.mapper.BeerMapper;
+import one.digitalinnovation.beerstock.repository.BeerRepository;
+import one.digitalinnovation.beerstock.service.BeerService;
+import org.junit.jupiter.api.Test;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
+public class BeerServiceTest {
+
+    @Mock
+    private BeerRepository beerRepository;
+
+    private BeerMapper beerMapper = BeerMapper.INSTANCE;
+
+    @InjectMocks
+    private BeerService beerService;
+
+    @Test
+    void whenBeerInformedThenShouldBeCreated() throws BeerAlreadyRegisteredException {
+        //given
+        //the method receive a DTO so we are going to use Bulder pattern that i used commonly in tests
+        BeerDTO beerDTO = BeerDTOBuilder.builder().build().toBeerDTO();
+        //we need to convert a DTO to a Beer
+        Beer expectedSavedBeer = beerMapper.toModel(beerDTO);
+
+        //when
+        when(beerRepository.findByName(beerDTO.getName())).thenReturn(Optional.empty());
+        when(beerRepository.save(expectedSavedBeer)).thenReturn(expectedSavedBeer);
+
+        //then
+        BeerDTO createdBeerDTO = beerService.createBeer(beerDTO);
+
+        assertEquals(beerDTO.getId(), createdBeerDTO.getId());
+        assertEquals(beerDTO.getName(), createdBeerDTO.getName());
+    }
+}
